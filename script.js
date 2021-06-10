741f5421b169efc3c69825af3cf5556a5e3ce657
Damon Rochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-10 15:04:46 +0000

    c

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let guess = 0
 
 while (guess !== number) {
   guess = prompt('Guess a number between 1 and 30')
+  guess = parseInt(guess)
   if (guess > number) {
     alert('Guess is TOO big')
   }
