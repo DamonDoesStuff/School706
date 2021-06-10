4f39b1f2a64aa9109dfd8198823610b67d80e436
Damon Rochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-10 14:42:06 +0000

    LLL

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let number = 15
+const number = 15
 let guess = 0
 
-while (guess != number) {
+while (guess !== number) {
   guess = prompt('Guess a number between 1 and 30')
   if (guess > number)
-alert("Guess is TOO big")
-if (guess < number)
-alert("Guess is TOO small")
+    alert('Guess is TOO big')
+  if (guess < number)
+    alert('Guess is TOO small')
 }
