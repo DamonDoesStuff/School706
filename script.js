011d50406a0c8a8e4ecb8f0b54beafe1f6ff057f
Damon Rochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-10 15:00:44 +0000

    t

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let guess = 0
 
 while (guess !== number) {
   guess = prompt('Guess a number between 1 and 30')
-  if (guess > number)
+  if (guess > number) {
     alert('Guess is TOO big')
-  if (guess < number)
+  }
+  if (guess < number) {
     alert('Guess is TOO small')
+  }
 }
